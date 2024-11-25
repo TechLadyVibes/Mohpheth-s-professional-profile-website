d4aaa0264d7476c060bb27e1cfd46bd7803609fa
Tech Lady Vibes <mohphethic@gmail.com>
2024-11-25 15:20:00 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/*.html", // Path to your HTML file(s)
+    "./src/*.{css,js,jsx,ts,tsx}", // Path to any JS/TS files if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
